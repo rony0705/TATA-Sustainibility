1d109d9f7bde8af79f348964ad6a2291ba608c45
mukherjeesrijan2 <srijanmukherjeemail@gmail.com>
2017-07-30 23:34:53 +0530

    Desktop Version

---
+++ desk/js/custom.js
@@ -0,0 +1,11 @@
+// $(window).ready(function () {
+// 	$('.controls li').mouseenter(function (e) {
+// 		var elemId = e.target.id;
+// 		$("#" + elemId + " span").css({'display': 'inline'});
+// 		$("#" + elemId).css({'width': 'auto'});
+// 	}).mouseleave(function (e) {
+// 		var elemId = e.target.id;
+// 		$("#" + elemId + " span").css({'display': 'none'});
+// 		$("#" + elemId).css({'width': '20px'});
+// 	})
+// })
